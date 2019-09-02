8ca1651cc6ae6b204fb27ff75f3e71d33b0b9aa3
Z. Drake <zeedrake3000@gmail.com>
2019-09-02 00:49:43 -0500

    Wrote scraper code for jeff parcels & push into csv

--- jefferson/county_scraper.rb
+++ jefferson/county_scraper.rb
@@ -2,15 +2,8 @@ require 'watir'
 require 'nokogiri'
 require 'httparty'
 require 'pry'
+require 'csv'
 
-# property lookups:
-# list from: https://landgrid.com
-# jefferson: http://199.48.41.18/AandTWebQuery/MainQueryPage.aspx?QueryMode=&Query=
-# -click on Map & TaxLot link on left
-# lane: http://apps.lanecounty.org/PropertyAccountInformation/#
-# elmore: http://idahoparcels.us:8080/gm3/desktop/elmore.html#on=sketch/default;scalebar_feet/scalebar_feet;parcels/parcels;parcels1/parcels1;boundry/boundry;bing/roads&loc=0.29858214173896974;-12881402.949012272;5324187.540158878
-#
-# public records: https://publicrecords.netronline.com/
 lot_numbs = Array.new
 
 f = File.open('test-parcel-ids.csv', 'r')
@@ -34,32 +27,90 @@ def insert_lot_number(num)
 
   # input number in form field
   browser.text_field(id: 'ctl00_ContentPlaceHolder1_WuctlQueryMapAndTaxlot1_WuctlMapAndTaxlot1_txtMapAndTaxlot').set "#{num}"
+
   browser.send_keys :return
 
-  binding.pry
+  # binding.pry
 
-  if browser.url.include?('Real/Index')
-    scraper(browser.url, browser)
+  if browser.url.include?('AccountID')
+    # view HTML of page - aka page source
+    # pass to nokogiri to parse
+    scraper(browser.html, browser)
   else
-    @scraper_data << [num, nil, nil, nil, nil,'SKIPPED']
+    @scraper_data << [num, nil,'SKIPPED']
     browser.quit
   end
 end
+
+def scraper(html, window)
+  # source = "view-source:" + url
+  # unparsed_page = HTTParty.get(source)
+  parsed_page = Nokogiri::HTML(html)
+  # binding.pry
+
+  #row data [parcel_id, acct_num, owner, mailing_addy, mailing_city+, 'JEFFERSON ', nil, nil, link-to-summary-report]
+  new_row = []
+
+  #get tax_lot/parcel_id
+  new_row << parsed_page.css('#ctl00_ContentPlaceHolder1_lblMapNumber').text
+
+  #get acct#,
+  new_row << parsed_page.css('#ctl00_ContentPlaceHolder1_lblAccountNumber').text
+
+  #get owner,
+  new_row << parsed_page.css('#ctl00_ContentPlaceHolder1_lblOwner').text
+
+  #get owner_mailing_addy,
+  full_o_addy = parsed_page.css('#ctl00_ContentPlaceHolder1_lblMailingAddress').text
+
+  broken_addy = full_o_addy.split(' ')
+  city_idx = broken_addy.find_index(broken_addy.find{|str| str.include?(',')})
+
+  # 1st pt of addy ~ 3 dots 2 leave out city
+  new_row <<  broken_addy[0...city_idx].join(' ')
+
+  # add city, state zip
+  new_row <<  broken_addy[city_idx..-1].join(' ')
+
+  # add coutny
+  new_row << 'JEFFERSON'
+
+  # adding nils 4 'Property Address', 'Property City, State Zip
+  2.times {new_row << nil}
+
+  # get link to 'summary report' which contians RMV & site addy (maybe)
+  part_summary_url = parsed_page.css('#ctl00_ContentPlaceHolder1_lnkReportLink1').attr('href').value.split('ReportViewer')[1]
+  full_summary_url = 'http://query.co.jefferson.or.us/AandTWebQuery/ReportViewer' + part_summary_url
+
+  # push summary_url into arr, has RVM & legal descrp
+  new_row << full_summary_url
+
+  #add acct status
+  new_row << parsed_page.css('#ctl00_ContentPlaceHolder1_lblAccountStatus').text
+  # binding.pry
+
+  @scraper_data << new_row
+
+  #close browser
+  window.quit
+end
+
+def csv_tool(h, data)
+  # CSV.open('generated_desCo_skipped_accts.csv', 'wb') do |csv|
+  CSV.open('generated_jeff_info.csv', 'wb') do |csv|
+    csv << h
+    data.each do |row|
+      csv << row
+    end
+  end
+end
+
 
 # binding.pry
 lot_numbs[1..-1].each do |parcel_id|
   insert_lot_number(parcel_id)
 end
 
-# def scrape_list
-#   url = '/us/or/jefferson/warm-springs#b=none&t=list'
-#   unparsed_page = HTTParty.get(url)
-#   parsed_page = Nokogiri::HTML(unparsed_page)
-#   binding.pry
-#   parsed_page.css('#pjax-container #waterfall').each do |node|
-#
-#   end
-#
-# end
+headers = ['Parcel ID', 'Account #', 'Last Name, First Name', 'Address', 'City, State Zip', 'Property County', 'Property Address', 'Property City, State Zip', 'RMV_Total', 'Additional Property Info']
 
-# scrape_list
+csv_tool(headers, @scraper_data)
