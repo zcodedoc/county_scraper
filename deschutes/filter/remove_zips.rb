bdcf9da87bf562a386a153b43367eef573581d83
Z. Drake <zeedrake3000@gmail.com>
2019-09-01 20:01:39 -0500

    moved files

---
+++ deschutes/filter/remove_zips.rb
@@ -0,0 +1,19 @@
+require 'pry'
+require 'csv'
+
+zips = %w(97701 97702 97707 97708 97709 97759 97712 97756 97739).uniq
+
+csv = CSV.read('Filter-List.csv', headers: :first_row, return_headers: true)
+# csv.by_row!
+csv.delete_if do |row|
+  # binding.pry
+  zips.include?(row.field('Zipcode').split('-').first) unless row.header_row?
+end
+
+CSV.open("Filter-List.csv","wb") do |csv_out|
+    # csv.by_row!
+    csv.each{ |row| csv_out << row }
+end
+
+#filter code minic
+# https://www.ruby-forum.com/t/csv-delete-an-entry/242928/3
---
+++ deschutes/skipped/get_acct_nums_scraper.rb
@@ -0,0 +1,54 @@
+require 'watir'
+require 'nokogiri'
+require 'httparty'
+require 'pry'
+require 'csv'
+
+skipped_taxlots = Array.new
+
+f = File.open('SKIPPED_Lots.csv', 'r')
+
+f.each_line {|line| skipped_taxlots << line.split("\r")[0] }
+
+@acct_numbers = []
+
+def insert_lot_number(num)
+  browser = Watir::Browser.new
+  browser.goto 'http://dial.deschutes.org/'
+  browser.text_field(id: 'value').set "#{num}"
+  browser.send_keys :return
+
+  get_acct_numbers(browser.url, browser)
+  browser.quit
+end
+
+def get_acct_numbers(url, window)
+  unparsed_page = HTTParty.get(url)
+  parsed_page = Nokogiri::HTML(unparsed_page)
+
+  parsed_page.css('tbody tr').each do |row|
+    unless row.css('td')[3].text.include?('USA') || row.css('td')[7].text.include?('Cancelled')
+      @acct_numbers << row.css('td')[2].text.split("\r\n ").each{|str| str.gsub!(/\s+/, '')}.delete_if{|el| el.length < 2}
+      # @acct_numbers.flatten!
+    end
+  end
+
+  # binding.pry
+end
+
+skipped_taxlots.each do |num|
+  # binding.pry
+  insert_lot_number(num)
+end
+
+
+def csv_tool(h, data)
+  CSV.open('scraped_accts_nums.csv', 'wb') do |csv|
+    csv << h
+    data.each do |row|
+      csv << row
+    end
+  end
+end
+
+csv_tool(['Account Number'], @acct_numbers)
