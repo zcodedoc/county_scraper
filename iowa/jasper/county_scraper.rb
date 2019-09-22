a095c9f2d5922b48dc696bea1e7dad25b2be4c3d
Z. Drake <zeedrake3000@gmail.com>
2019-09-22 11:04:55 -0500

    add rescue & authenticate method to auth with sever to be able to pull data

--- iowa/jasper/county_scraper.rb
+++ iowa/jasper/county_scraper.rb
@@ -8,71 +8,71 @@ require_relative 'create_csv'
 @scraper_data = []
 
 def get_county_tables(web, url)
-  # browser = Watir::Browser.new
-  web.goto url
-
-  #creating url for final page
-  base_url = 'https://jasper.iowaassessors.com/'
-  parsed_page = Nokogiri::HTML(web.html)
-  final_page_url =  base_url + parsed_page.css('div#navButtons a').last.attr('href')
-
-  while web.url != final_page_url
-    scrape_apns(web.html)
-
-    # find a tag w/img alt as next page
-    a_tag = parsed_page.css('div#navButtons a').find{|node| node.css('img').attr('alt').value == "Next Page "}
+  begin
+    web.goto url
+    #creating url for final page
+    base_url = 'https://jasper.iowaassessors.com/'
+    parsed_page = Nokogiri::HTML(web.html)
 
     # binding.pry
-    # create url & g2 next page
-    next_page_url = base_url + a_tag.attr('href')
-    get_county_tables(web, next_page_url)
-    # use a tag 2g2 next page
-    # browser.goto
+    if !parsed_page.css('div#navButtons a').empty?
+
+      # url id different then finall webpages, conditional didn't work
+      # final_page_url =  base_url + parsed_page.css('div#navButtons a').last.attr('href')
+      # while web.url != final_page_url
+
+      # potential fix
+      final_page_img_text = parsed_page.css('div#navButtons a').last.css('img').attr('alt').value
+
+      # binding.pry
+      while final_page_img_text != "Previous Page " #check for this b/c there's NO nxt page
+        scrape_apns(web.html)
+
+        # find a tag w/img alt as next page
+        a_tag = parsed_page.css('div#navButtons a').find{|node| node.css('img').attr('alt').value == "Next Page "}
+
+        # binding.pry
+        # create url & g2 next page
+        next_page_url = base_url + a_tag.attr('href')
+        get_county_tables(web, next_page_url)
+      end
+
+      # scrape last page from site
+      scrape_apns(web.html)
+
+      web.quit
+    else
+      authenticate_browser(web, url)
+    end
+  rescue Watir::Exception::UnknownObjectException
+    # close window
+    web.quit
+
+    # open new window
+    browser = Watir::Browser.new
+
+    # binding.pry
+    # send new window to be authenticated in
+    authenticate_browser(browser, url)
   end
-
-  scrape_apns(web.html)
-
-  web.quit
-  # ['Parcel Number', 'Property Address', 'Property City, State Zip', 'Lot Area', 'Appraised Value']
-
-  # browser.text_field(id: 'value').set "#{num}"
-  # browser.send_keys :return
-  # binding.pry
-  # if browser.url.include?('Real/Index')
-  #   scraper(browser.url, browser)
-  # else
-  #   @scraper_data << [num, 'SKIPPED']
-  #   browser.quit
-  # end
 end
 
-def scrape_apns(html)
-  parsed_page = Nokogiri::HTML(html)
-
-  # each pages table
-  parsed_page.css('tbody tr')[1..-1].each do |tr|
-    new_data = []
-
-    #parcel Number
-    num = tr.css('td a').text
-
-    unless num == ''
-      new_data << num
-
-      #page number (if errors/shuts down)
-      new_data << parsed_page.css('div#navButtons span').first.text.strip
-
-      @scraper_data << new_data
-    end
-  end
-
+def authenticate_browser(webpage, cpu_addy)
   # binding.pry
+  authenticate_site = "https://jasper.iowaassessors.com/showResSaleSearch.php"
+  webpage.goto authenticate_site
 
-  # save scrapped apns
-  csv_tool(h = nil, @scraper_data.uniq)
+  # click agree button for disclaimer form
+  webpage.input(name: "agree").click
 
-  #clear array for next page
-  @scraper_data.clear
+  sleep(2)
+  # g2 url that creates tables of VL to view
+  webpage.goto "https://jasper.iowaassessors.com/showVacantSaleSearch.php"
+
+  #click search display results
+  webpage.input(type: "submit").click
+  # binding.pry
+  get_county_tables(webpage, cpu_addy)
 end
 
 # def table_scraper(html, webpage)
@@ -128,4 +128,4 @@ def parcel_scraper(parcel_html, data)
 end
 
 browser = Watir::Browser.new
-get_county_tables(browser, "https://jasper.iowaassessors.com/results.php?sort_options=0&sort=0&mode=vacantsale&sale_date1=&sale_date2=&sale_amt1=&sale_amt2=&recording1=&ilegal=&nutc1=21%2C34%2C137%2C153%2C321%2C334&ttlacre1=&ttlacre2=&location1=&class1=&maparea1=&dist1=&appraised1=&appraised2=")
+authenticate_browser(browser, "https://jasper.iowaassessors.com/results.php?sort_options=0&sort=0&mode=vacantsale&sale_date1=&sale_date2=&sale_amt1=&sale_amt2=&recording1=&ilegal=&nutc1=21%2C34%2C137%2C153%2C321%2C334&ttlacre1=&ttlacre2=&location1=&class1=&maparea1=&dist1=&appraised1=&appraised2=")
